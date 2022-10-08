b9fa9032e35d0d194b5017f4fdf5eb7b1fa2bfe1
Andy García <>
2022-10-08 17:31:10 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+#Data types
+
+#String
+var="Hello"[4]
+
+print("Hello"[4])
+
+
+#Integer
+varInt =123
+print(123+345)
+
+#FloatingPointError
+varFloat = 3.4134
+print(varFloat)
+
+#Boolean
+True
+False
+
+# Data Type Casting
+num_char = len(input("What is your name?"))
+new_num_char = str(num_char)
+print("Your name has " + new_num_char + " characters.")
+#print(type(num_char))
